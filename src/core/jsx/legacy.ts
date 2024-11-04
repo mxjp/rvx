6db29c3fb264ed394d75b3f0d3813df2c21e1848
Max J. Polster <max.polster@id-p.de>
2024-11-04 21:21:50 +0100

    Fix legacy runtime types

--- src/core/jsx/legacy.ts
+++ src/core/jsx/legacy.ts
@@ -1,6 +1,24 @@
+import type { Attributes, TagNameMap } from "../element-common.js";
 import { createElement } from "./internals.js";
 
-export * from "./types.js";
+type NativeElement = Element;
+
+declare global {
+	export namespace JSX {
+		export type IntrinsicElements = {
+			[K in keyof TagNameMap]: Attributes<TagNameMap[K]>;
+		} & {
+			[K in string]: Attributes<NativeElement>;
+		};
+
+		export interface ElementChildrenAttribute {
+			children: {};
+		}
+
+		export type Element = unknown;
+		export type ElementClass = never;
+	}
+}
 
 export const Fragment = Symbol.for("rvx:jsx-fragment");
 
@@ -14,7 +32,7 @@ export function jsx(type: any, props: any, ...children: any[]): unknown {
 		if (children.length > 0) {
 			props.children = children;
 		}
-		return type(props);
+		return type(props ?? NO_PROPS);
 	}
 	return createElement(type, props ?? NO_PROPS, children);
 }
--- src/core/jsx/r17.ts
+++ src/core/jsx/r17.ts
@@ -1,6 +1,22 @@
+import type { Attributes, TagNameMap } from "../element-common.js";
 import { createElement } from "./internals.js";
 
-export * from "./types.js";
+type NativeElement = Element;
+
+export namespace JSX {
+	export type IntrinsicElements = {
+		[K in keyof TagNameMap]: Attributes<TagNameMap[K]>;
+	} & {
+		[K in string]: Attributes<NativeElement>;
+	};
+
+	export interface ElementChildrenAttribute {
+		children: {};
+	}
+
+	export type Element = unknown;
+	export type ElementClass = never;
+}
 
 export const Fragment = Symbol.for("rvx:jsx-fragment");
 
