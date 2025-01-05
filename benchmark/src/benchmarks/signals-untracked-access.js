5f270c1cc7cac5556ff5750c46d402113a49fae3
Max J. Polster <max.polster@id-p.de>
2025-01-05 12:49:23 +0100

    Add more signal benchmarks

---
+++ benchmark/src/benchmarks/signals-untracked-access.js
@@ -0,0 +1,12 @@
+
+export const multiplier = 1000;
+
+/** @param {import("rvx")} */
+export function create({ sig }) {
+	return () => {
+		const signal = sig(0);
+		for (let i = 0; i < multiplier; i++) {
+			signal.access();
+		}
+	};
+}
