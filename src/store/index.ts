a655104f3259ec2a3fb66683bd8239d8b625739a
Max J. Polster <max.polster@id-p.de>
2025-02-27 01:27:14 +0100

    Add missing export

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,5 +2,6 @@ export * from "./barrier.js";
 export * from "./probes.js";
 export * from "./reactive-array-proxy.js";
 export * from "./reactive-map.js";
+export * from "./reactive-proxy.js";
 export * from "./reactive-set.js";
 export * from "./reflect.js";
