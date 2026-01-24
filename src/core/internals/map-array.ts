9f83cf958a0597102d9b92db30523f9d9a8f70f1
Max J. Polster <max.polster@id-p.de>
2026-01-24 16:29:41 +0100

    Insert only fast path

--- src/core/internals/map-array.ts
+++ src/core/internals/map-array.ts
@@ -49,6 +49,21 @@ export function createMapArrayState<I, O>() {
 	return state;
 }
 
+function createEntry<I, O>(value: I, index: number, fn: MapArrayFn<I, O>): MapArrayStateEntry<I, O> {
+	const signal = $(index);
+	let output!: O;
+	const dispose = isolate(capture, () => {
+		output = fn(value, () => signal.value);
+	});
+	return {
+		i: value,
+		o: output,
+		s: signal,
+		d: dispose,
+		r: false,
+	};
+}
+
 export function mapArrayUpdate<I, O>(state: MapArrayStateEntry<I, O>[], inputs: I[], fn: MapArrayFn<I, O>): MapArrayUpdate<I, O> | null {
 	let start = 0;
 	const maxStart = Math.min(state.length, inputs.length);
@@ -62,7 +77,6 @@ export function mapArrayUpdate<I, O>(state: MapArrayStateEntry<I, O>[], inputs: 
 
 	const minEnd = inputs.length - maxStart + start;
 	const lenDiff = inputs.length - state.length;
-
 	let end = inputs.length - 1;
 	while (end >= minEnd && Object.is(inputs[end], state[end - lenDiff].i)) {
 		end--;
@@ -70,62 +84,51 @@ export function mapArrayUpdate<I, O>(state: MapArrayStateEntry<I, O>[], inputs: 
 	end++;
 	const stateEnd = end - lenDiff;
 
-	if ((end - lenDiff - start) === 0) {
-		// TODO: Insert only fast path.
-	}
-
-	const indexByValue = new Map<I, number>();
-	const nextIndexByIndex: number[] = [];
-	for (let i = end - 1; i >= start; i--) {
-		const value = inputs[i];
-		const next = indexByValue.get(value);
-		if (next !== undefined) {
-			nextIndexByIndex[i] = next;
-		}
-		indexByValue.set(value, i);
-	}
-
 	const nextState: MapArrayStateEntry<I, O>[] = [];
-
-	for (let i = start; i < stateEnd; i++) {
-		const instance = state[i];
-		const index = indexByValue.get(instance.i);
-		if (index === undefined) {
-			instance.d();
-			instance.r = true;
-		} else {
-			nextState[index - start] = instance;
-			indexByValue.set(instance.i, nextIndexByIndex[index]);
+	if ((end - lenDiff - start) === 0) {
+		for (let i = start; i < end; i++) {
+			nextState[i - start] = createEntry(inputs[i], i, fn);
 		}
-	}
-
-	for (let i = start; i < end; i++) {
-		const old = nextState[i - start];
-		if (old) {
-			old.s.value = i;
-		} else {
+	} else {
+		const indexByValue = new Map<I, number>();
+		const nextIndexByIndex: number[] = [];
+		for (let i = end - 1; i >= start; i--) {
 			const value = inputs[i];
+			const next = indexByValue.get(value);
+			if (next !== undefined) {
+				nextIndexByIndex[i] = next;
+			}
+			indexByValue.set(value, i);
+		}
 
-			const index = $(i);
-			let output!: O;
-			const dispose = isolate(capture, () => {
-				output = fn(value, () => index.value);
-			});
+		for (let i = start; i < stateEnd; i++) {
+			const instance = state[i];
+			const index = indexByValue.get(instance.i);
+			if (index === undefined) {
+				instance.d();
+				instance.r = true;
+			} else {
+				nextState[index - start] = instance;
+				indexByValue.set(instance.i, nextIndexByIndex[index]);
+			}
+		}
 
-			nextState[i - start] = {
-				i: value,
-				o: output,
-				s: index,
-				d: dispose,
-				r: false,
-			};
+		for (let i = start; i < end; i++) {
+			const old = nextState[i - start];
+			if (old) {
+				old.s.value = i;
+			} else {
+				nextState[i - start] = createEntry(inputs[i], i, fn);
+			}
 		}
 	}
 
 	const prevState = state.splice(start, stateEnd - start, ...nextState);
 
-	for (let i = end; i < state.length; i++) {
-		state[i].s.value = i;
+	if (stateEnd !== end) {
+		for (let i = end; i < state.length; i++) {
+			state[i].s.value = i;
+		}
 	}
 
 	return {
