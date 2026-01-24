4e786ca42993c0d6ed548d96d9b8cd43204d0177
Max J. Polster <max.polster@id-p.de>
2026-01-24 18:49:32 +0100

    fix tracking

--- src/core/map-array.ts
+++ src/core/map-array.ts
@@ -1,5 +1,5 @@
 import { createMapArrayState, MapArrayFn, mapArrayUpdate } from "./internals/map-array.js";
-import { $, Expression, get, watch } from "./signals.js";
+import { $, Expression, get, untrack, watch } from "./signals.js";
 
 export { MapArrayFn } from "./internals/map-array.js";
 
@@ -9,7 +9,7 @@ export function mapArray<I, O>(inputs: Expression<Iterable<I>>, fn: MapArrayFn<I
 	watch(() => {
 		const update = mapArrayUpdate(state, get(inputs), fn);
 		if (update !== null) {
-			output.value.splice(update.s, update.p.length, ...update.n.map(s => s.o));
+			untrack(output).splice(update.s, update.p.length, ...update.n.map(s => s.o));
 			output.notify();
 		}
 	});
