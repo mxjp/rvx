37e3fd971dc2ce3896ac8241468b5843c131b493
Max J. Polster <max.polster@id-p.de>
2026-05-08 22:09:04 +0200

    Fix build.

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -1,14 +1,27 @@
 import terser from "@rollup/plugin-terser";
-import { readFile } from "node:fs/promises";
 import { dirname, isAbsolute, join, relative, resolve } from "node:path";
 import { brotliCompressSync, gzipSync } from "node:zlib";
 import { rollup } from "rollup";
 import { dts } from "rollup-plugin-dts";
 import { bundleName, dist, moduleNames, root, tscOut } from "./common.js";
 
-const license = await readFile(join(root, "LICENSE"), "utf-8");
+const license = `
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+`;
+
 const bannerPlugin = {
-	banner: `/*!\n${license}*/`,
+	banner: `/*!${license}*/`,
 };
 
 const formatPlugin = {
@@ -21,7 +34,7 @@ const formatPlugin = {
 				if (/\t (?!\*)/m.test(code)) {
 					throw new Error("output indentation is broken");
 				}
-				asset.code = code;
+				asset.code = code.trimStart();
 			}
 		}
 	},
